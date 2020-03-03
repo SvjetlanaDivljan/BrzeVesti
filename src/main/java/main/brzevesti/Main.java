1e2c3214a22c6493b9365fe2b5e4cef02e5a79ad
Svjetlana Divljan <svjetlana.divljan@gmail.com>
2020-03-03 20:21:35 +0100

    Brze Vesti

---
+++ src/main/java/main/brzevesti/Main.java
@@ -0,0 +1,50 @@
+
+package main.brzevesti;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+
+       System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+       WebDriver driver = new ChromeDriver();
+       driver.get("http://bvtest.school.cubes.rs/login");
+       
+        WebElement emailField = driver.findElement(By.name("email"));
+        emailField.sendKeys("qa@cubes.rs");
+        
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("cubesqa");
+        
+        WebElement loginButton = driver.findElement(By.className("btn-primary"));
+        loginButton.click();
+        
+        System.out.println(driver.getTitle());
+        
+        WebElement signaturesLink = driver.findElement(By.linkText("Signatures"));
+        signaturesLink.click();
+        
+        System.out.println(driver.getTitle());
+        
+        WebElement categoriesLink = driver.findElement(By.partialLinkText("egories"));
+        categoriesLink.click();
+        
+        System.out.println(driver.getTitle());
+        
+        
+        
+        
+        //signaturesLink
+        //signaturesLink click
+        //console ->title
+        
+        
+    }
+    
+}
